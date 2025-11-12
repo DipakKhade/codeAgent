0be6616510836881f89cc290279aee6208cfd873
Dipak Khade <dipakhade214@gmail.com>
2025-11-12 23:21:41 +0530

    create file tool args achnages

--- tools/create_file.py
+++ tools/create_file.py
@@ -1,9 +1,8 @@
 from langchain_core.tools import tool
 
 @tool
-def create_file(file_name: str, file_path: str) -> str :
-    """this function take the file name and the file path as the input and create the file at the provided file path and return the path of created file"""
-    print("calling me -------------------")
-    with open(f"{file_path}/{file_name}", 'w') as file:
+def create_file(file_path: str) -> str :
+    """this function take the file path as the input and create the file at the provided file path and return the path of created file"""
+    with open(f"{file_path}", 'w') as file:
         pass
-    return f"{file_name}/{file_path}"
+    return file_path
